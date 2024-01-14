64a6daf8f87e55bf714cd3c02cee894a9bc6327c
MShinskiy <88272807+MShinskiy@users.noreply.github.com>
2024-01-14 22:35:53 +0300

    fill in order fields

--- src/main/java/com/imse/ticketshop/entity/Order.java
+++ src/main/java/com/imse/ticketshop/entity/Order.java
@@ -1,4 +1,22 @@
 package com.imse.ticketshop.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name = "orders")
+@Entity
+@Data
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    private LocalDateTime dateIssued;
+    private double price;
+    private String ticketNumber;
+
 }
