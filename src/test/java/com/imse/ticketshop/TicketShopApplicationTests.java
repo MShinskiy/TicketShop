6eca83c49a9d8c3767cfbb79973e331b1cb65afd
MShinskiy <88272807+MShinskiy@users.noreply.github.com>
2024-01-14 22:10:14 +0300

    Initial commit

---
+++ src/test/java/com/imse/ticketshop/TicketShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imse.ticketshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
